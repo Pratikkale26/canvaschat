b9f157f7c30678548cd9be353e99c05361f4294e
Pratik Kale <147796613+PKBOSS26@users.noreply.github.com>
2025-01-21 18:46:41 +0530

    some end points

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,66 @@
 import express, { Request, Response } from "express"
+import Jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import {CreateUserSchema, CreateRoomSchema, SigninSchema} from "@repo/common/types"
 
 const app = express();
 const port = 3001;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("hello this is pratik");
+app.post('/signup', (req:Request, res:Response) => {
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            messge: "incorrect inputs"
+        })
+        return;
+    }
+
+    // db call 
+    res.json({
+        userId: "4545"
+    })
+
 })
+
+app.post('/signin', (req: Request, res: Response) => {
+    const data = SigninSchema.safeParse(req.body)
+    if(!data.success){
+        res.json({
+            message: "incorrect Inputs"
+        })
+        return
+    }
+    res.send("hello this is pratik");
+
+    const userId = 1;
+    const token = Jwt.sign({
+        userId
+    }, JWT_SECRET)
+
+    res.json({
+        token
+    })
+})
+
+app.post('/room', middleware, (req: Request, res: Response) => {
+    const data = CreateRoomSchema.safeParse(req.body)
+    if(!data.success){
+        res.json({
+            message: "incorrect Inputs"
+        })
+        return
+    }
+    res.send("hello this is pratik");
+    // db call
+
+    res.json({
+        roomId: 345
+    })
+})
+
+
 
 
 app.listen(port, () => {
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,4 +1,6 @@
 import { WebSocketServer } from "ws";
+import Jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 // create the ws server
 const wss = new WebSocketServer({port: 8080})
@@ -6,15 +8,27 @@ const wss = new WebSocketServer({port: 8080})
 console.log(`ws server is running on: ws://localhost:8080`)
 
 //handle client 
-wss.on('connection', (ws) => {
+wss.on('connection', function connection(ws, request) {
     console.log("new client connected")
 
+    const url = request.url;
+    if(!url) {
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1])
+    const token = queryParams.get('token') ?? ""
+    const decoded = Jwt.verify(token, JWT_SECRET)
+    
+    if(!decoded || !(decoded as JwtPayload).userId) {
+        ws.close();
+        return
+    }
     //handle msg from the client
-    ws.on('message', (message) => {
-        console.log(`recieved the msg: ${message}`)
+    ws.on('message', function message(data) {
+        console.log(`recieved the msg: ${data}`)
 
         // echo the msg to the client
-        ws.send(`you said: ${message}`)
+        ws.send(`you said: ${data}`)
     })
 
     ws.on('close', () => {
